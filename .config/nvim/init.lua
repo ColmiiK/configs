2ee0d45f782e97607ea2a082a9e4353a779352bf
Álvaro Vega Gómez <alvarvg@gmail.com>
2025-04-12 23:00:23 +0200

    macros

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,24 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd.colorscheme("catppuccin")
+
+------------
+-- Macros --
+------------
+
+local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+
+vim.fn.setreg(
+	"l",
+	"yoconsole.log('" .. esc .. "pa:" .. esc .. "la, " .. esc .. "p"
+)
+vim.fn.setreg(
+	"k",
+	"yoassert("
+		.. esc
+		.. "pa !== undefined, '"
+		.. esc
+		.. "pa must exist"
+		.. esc
+		.. ""
+)
