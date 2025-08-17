4160b8ec6ca3e8ff9c6303a4f835cb03392cb150
Álvaro Vega Gómez <alvarvg@gmail.com>
2025-08-17 22:40:45 +0200

    split into files

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,11 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-require("config.jdtls")
-vim.cmd.colorscheme("catppuccin")
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
@@ -123,10 +127,3 @@ vim.api.nvim_create_autocmd("FileType", {
 		)
 	end,
 })
-
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>cb",
-	"o```<CR>```<Esc>kA",
-	{ noremap = true, silent = true, desc = "Insert Markdown code block" }
-)
