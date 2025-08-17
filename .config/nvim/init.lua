d6481b6ddf9bd25c3aed36185323684f95039538
Álvaro Vega Gómez <alvarvg@gmail.com>
2025-08-17 22:32:08 +0200

    obsidian java notes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,40 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("config.jdtls")
 vim.cmd.colorscheme("catppuccin")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client and client.name == "jdtls" then
+			-- Override diagnostics handler only for jdtls
+			client.handlers = client.handlers or {}
+			client.handlers["textDocument/publishDiagnostics"] = function(
+				err,
+				result,
+				ctx,
+				config
+			)
+				if result and result.diagnostics then
+					result.diagnostics = vim.tbl_filter(function(diagnostic)
+						return not (
+							diagnostic.message
+							and diagnostic.message:match(
+								"does not match the expected package"
+							)
+						)
+					end, result.diagnostics)
+				end
+				vim.lsp.diagnostic.on_publish_diagnostics(
+					err,
+					result,
+					ctx,
+					config
+				)
+			end
+		end
+	end,
+})
 
 ------------
 -- Macros --
@@ -69,3 +103,30 @@ vim.api.nvim_create_autocmd("FileType", {
 		)
 	end,
 })
+
+-- Java debug print
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "java",
+	callback = function()
+		local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+
+		vim.fn.setreg(
+			"l",
+			'yoSystem.out.println("'
+				.. esc
+				.. "pa: "
+				.. esc
+				.. "la + "
+				.. esc
+				.. "p$a;"
+				.. esc
+		)
+	end,
+})
+
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>cb",
+	"o```<CR>```<Esc>kA",
+	{ noremap = true, silent = true }
+)
