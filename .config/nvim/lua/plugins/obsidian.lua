c765510e1723437505d6d4598b9f67572083eb2d
Álvaro Vega Gómez <alvarvg@gmail.com>
2025-08-18 11:44:48 +0200

    updated frontmatter

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,21 +1,10 @@
 return {
 	"epwalsh/obsidian.nvim",
-	version = "*", -- recommended, use latest release instead of latest commit
+	version = "*",
 	lazy = true,
 	ft = "markdown",
-	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-	-- event = {
-	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-	--   -- refer to `:h file-pattern` for more examples
-	--   "BufReadPre path/to/my-vault/*.md",
-	--   "BufNewFile path/to/my-vault/*.md",
-	-- },
 	dependencies = {
-		-- Required.
 		"nvim-lua/plenary.nvim",
-
-		-- see below for full list of optional dependencies 👇
 	},
 	opts = {
 		workspaces = {
@@ -28,7 +17,22 @@ return {
 				path = "~/f/vaults/work",
 			},
 		},
+		ui = { enable = false },
+		templates = {
+			folder = "templates",
+			date_format = "%d-%m-%Y",
+			time_format = "%H:%M",
+		},
 
-		-- see below for full list of options 👇
+		-- 👇 this ensures frontmatter always has a date
+		note_frontmatter_func = function(note)
+			local out = {
+				id = note.id,
+				aliases = note.aliases,
+				tags = note.tags,
+				date = os.date("%d-%m-%Y %H:%M"), -- matches your template format
+			}
+			return out
+		end,
 	},
 }
